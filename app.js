be3119b8fb8c2dea1a41fcc34f996b0759354dc5
yoni4566 <ksy891119@gmail.com>
2021-08-02 22:15:42 +0900

    init js,html,css

---
+++ app.js
@@ -0,0 +1,13 @@
+//const, let
+const a = 5;
+const b = 2;
+let myName = "nico";
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hello " + myName);
+
+myName = "nicolas";
+console.log("your new name is " + myName);
+
+
